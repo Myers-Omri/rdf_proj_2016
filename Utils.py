3c5fcd9e0c134a3425506e5aad8846f78c1681ec
Myers-Omri <omri.myers@gmail.com>
2017-03-11 21:48:34 +0200

    added f to get rules

--- Utils.py
+++ Utils.py
@@ -32,7 +32,7 @@ dictionaries = [subjectsPerson, subjectsPlaces, subjectsLive]
 
 dictionariest = [{"Company": "http://dbpedia.org/ontology/Company",
                   'comedian': "http://dbpedia.org/ontology/Comedian",
-                  'Fish': "http://dbpedia.org/ontology/Fish",
+                  'Mammal': "http://dbpedia.org/ontology/Mammal",
                   "EducationalInstitution": "http://dbpedia.org/ontology/EducationalInstitution",
                   'politician': "http://dbpedia.org/ontology/Politician",
                   'architectural_structure': "http://dbpedia.org/ontology/ArchitecturalStructure"}]
@@ -40,3 +40,29 @@ dictionariest = [{"Company": "http://dbpedia.org/ontology/Company",
 dictionariesq = [{'comedian': "http://dbpedia.org/ontology/Comedian",
                   "EducationalInstitution": "http://dbpedia.org/ontology/EducationalInstitution",
                     'Play': "http://dbpedia.org/ontology/Play"}]
+
+
+if __name__ == '__main__':
+    import smtplib
+
+    # Import the email modules we'll need
+    from email.mime.text import MIMEText
+
+    # Open a plain text file for reading.  For this example, assume that
+    # the text file contains only ASCII characters.
+    #fp = open(textfile, 'rb')
+    # Create a text/plain message
+    msg = MIMEText("just checking emails")
+    #fp.close()
+
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = 'The contents of subj'
+    msg['From'] = 'omri.myers@gmail.com'
+    msg['To'] = 'omri.myers@gmail.com'
+
+    # Send the message via our own SMTP server, but don't include the
+    # envelope header.
+    s = smtplib.SMTP('localhost')
+    s.sendmail('omri.myers@gmail.com', ['omri.myers@gmail.com'], msg.as_string())
+    s.quit()
